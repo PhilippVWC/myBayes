2a9e1a7e1d037843e075661a4bcaae27e3de4a63
PhilippVWC <apfelphil@googlemail.com>
2019-12-18 23:40:40 +0100

    iteratedMap function added

--- R/iteratedMap.R
+++ R/iteratedMap.R
@@ -1,34 +1,65 @@
-#' @title Time series creation
-#' @param x0 double - starting value
-#' @param r double - bifurcation parameter
-#' @param n integer - length of resulting time series
-#' @param funct function of starting value x0 - the corresponding map
-#' @details This routine takes a map an applies it iteratively
-#' @return vector of type double and length n, i.e. the resulting time series.
+#' @title Time series creation by given map.
+#' @param x0 double - initial condition.
+#' @param n integer - length of resulting time series.
+#' @param fun function of one parameter - the corresponding map.
+#' @param skipfirst logical - wether the initial condition is to be removed.
+#' @details This routine takes a map and applies it iteratively.
+#' @return vector of type double and length n - the resulting time series.
+#' @author Philipp van Wickevoort Crommelin
 #' @examples
-#' r0 = 0.918999
-#' x0 = 0.5
-#' N = 100
-#' y = iteratedMap(x = x0,
-#'                 r = r0,
-#'                 n = N,
-#'                 funct = function(x,r) myBayes::gsm_cpp(x,
-#'                                                        r,
-#'                                                        alpha = 2.0))
-#' plot(x = 1:N,
-#'      y = y,
-#'      ylim = c(0,1),
-#'      type = "l")
+#'N = 12
+#'x = seq(from = 0,
+#'        to = 1,
+#'        by = 1/(N-1))
+#'y = stats::runif(N)
+#'fun = stats::approxfun(x = x,
+#'                       y = y,
+#'                       method = "linear")
+#'x0 = 0.2
+#'par(mfrow = c(2,1))
+#'plot(x = x,
+#'     y = y,
+#'     main = "map",
+#'     type = "l",
+#'     col = "lightblue",
+#'     lwd = 7,
+#'     ylab = "")
+#'N_series = 30
+#'tmser = myBayes::iteratedMap(n = N_series,
+#'                    x = x0,
+#'                    fun = fun,
+#'                    skipFirst = FALSE)
+#'plot(NULL,
+#'     xlim = c(1,N_series),
+#'     ylim = c(1,0),
+#'     main = "Time series",
+#'     xlab = "iteration",
+#'     ylab = "")
+#'lines(x = 1:N_series,
+#'      y = tmser,
+#'      col = "purple",
+#'      lwd = 5,
+#'      ylab = "")
+#'points(x = 1:N_series,
+#'       y = tmser,
+#'       col = "orange",
+#'       cex = 2,
+#'       pch = 16)
 #' @export
-iteratedMap = function(x,
-                       r,
-                       n,
-                       funct){
-  S = x
-  if(n>1){
-    for (i in 2:n){
-      S = c(S,funct(S[i-1],r))
+iteratedMap = function(n,x0,fun,skipFirst = TRUE){
+  X = rep(0,n)
+  if(skipFirst){
+    X[1] = fun(x0)
+  }else{
+    X[1] = x0
+  }
+  if(n>=2){
+    for(i in 2:n){
+      X[i] = fun(X[i-1])
     }
   }
-  return(S)
+  return(X)
 }
+
+
+
---
+++ iterMap.R
@@ -0,0 +1,34 @@
+#' @title Time series creation
+#' @param x0 double - starting value
+#' @param r double - bifurcation parameter
+#' @param n integer - length of resulting time series
+#' @param funct function of starting value x0 - the corresponding map
+#' @details This routine takes a map and applies it iteratively
+#' @return vector of type double and length n, i.e. the resulting time series.
+#' @examples
+#' r0 = 0.918999
+#' x0 = 0.5
+#' N = 100
+#' y = iteratedMap(x = x0,
+#'                 r = r0,
+#'                 n = N,
+#'                 funct = function(x,r) myBayes::gsm_cpp(x,
+#'                                                        r,
+#'                                                        alpha = 2.0))
+#' plot(x = 1:N,
+#'      y = y,
+#'      ylim = c(0,1),
+#'      type = "l")
+#' @export
+iterMap = function(x,
+                       r,
+                       n,
+                       funct){
+  S = x
+  if(n>1){
+    for (i in 2:n){
+      S = c(S,funct(S[i-1],r))
+    }
+  }
+  return(S)
+}
